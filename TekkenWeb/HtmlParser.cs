9e0dba489c99cb1556f6ef51186a0d433cc3d576
budi0102 <dari_saya@hotmail.com>
2017-06-17 19:27:10 +0900

    Reconstructing the projects

---
+++ TekkenWeb/HtmlParser.cs
@@ -0,0 +1,46 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekkenWeb
+{
+    public class HtmlParser
+    {
+        /// <summary>
+        /// Singleton Instance
+        /// </summary>
+        public static HtmlParser Instance { get; private set; }
+
+        #region Constructor
+        /// <summary>
+        /// Static Constructor
+        /// </summary>
+        static HtmlParser()
+        {
+            Instance = new HtmlParser();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public HtmlParser()
+        {
+            HtmlDocument = new HtmlDocument();
+        }
+        #endregion
+
+        #region Public Properties
+        public HtmlDocument HtmlDocument { get; set; }
+        #endregion
+
+        #region Public Methods
+        public void Load(string htmlString)
+        {
+            HtmlDocument.LoadHtml(htmlString);
+        }
+        #endregion
+    }
+}
---
+++ TekkenWeb/WebAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace TekkenWeb
+{
+    public class WebAPI
+    {
+        public HttpClient Client { get; set; }
+
+        public WebAPI()
+        {
+            Client = new HttpClient();
+        }
+
+        public async Task<string> GetAsync(string url)
+        {
+            Uri uri = new Uri(url);
+            return await Client.GetStringAsync(uri);
+        }
+
+    }
+}
